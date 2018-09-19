d814fb12b12fe247e3247f26aa078b3ffa933696
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2018-09-19 16:34:57 -0400

    Update file1.py

--- file1.py
+++ file1.py
@@ -4,5 +4,12 @@ import math
 import threading
 import os
 root = Tk()
-canvas=(root, width=700, height=700 fill="green")
+canvas=(root, width=700, height=700 bg="green")
 canvas.pack(expand=YES, fill=BOTH)
+c = canvas.create_rectangle(root, 0, 0, 50, 50)
+canvas.move(c, 50, 50)
+def wow():
+  canvas.move(c, 50, 0)
+b = Button(root, text="Move Up", command=wow)
+b.pack()
+root.mainloop()
