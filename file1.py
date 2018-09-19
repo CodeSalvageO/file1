b8871a1388b3149246dae9c34792bf5b58602b8d
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2018-09-19 16:28:00 -0400

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import messagebox
+import math
+import threading
+import os
+root = Tk()
+canvas=(root, width=700, height=700 fill="green")
+canvas.pack(expand=YES, fill=BOTH)
